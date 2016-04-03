6fc147c4dd62dafb065f7b392dd032b897ccf7ee
Ariane Deschenes Foucher <ariane.df@me.com>
2016-04-03 13:20:24 -0400

    Finished calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -7,6 +7,23 @@ def subtract(a, b)
 end
 
 def sum(array)
-  array = []
-  array.inject(0) {|sum, x| sum += x}
+  array.inject(0) do |sum, x| sum += x
+  end
+end
+
+def product(array)
+  final = 1.0
+  array.each do |i|
+    final *= i
+  end
+  final
+end
+
+def factorial(number)
+  if number == 0
+    return 1
+  elsif number == 1 || number == 2
+    return number
+  else return number * factorial(number -1)
+  end
 end
